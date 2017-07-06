98d4c777911224dd4d3e19eebf0e52d5eb107446
Ankur Patel <apatel@shutterstock.com>
2017-07-06 15:49:37 -0400

    Updating Sample

--- Package.swift
+++ Package.swift
@@ -1,8 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "Test",
+    name: "Sample",
     dependencies: [ 
-        .Package(url: "https://github.com/ankurp/Dollar", majorVersion: 5, minor: 0),
-        .Package(url: "https://github.com/ankurp/Cent", majorVersion: 5, minor: 0)
+        .Package(url: "https://github.com/ankurp/Dollar", majorVersion: 6, minor: 2),
     ])
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,3 +1,12 @@
 import Dollar
 
-print("$.map([1, 2, 3]) { $0 * 10 } = \($.map([1, 2, 3]) { $0 * 10 })")
+// Mapping
+print($.map([1, 2, 3]) {
+  $0 * 10
+})
+
+// Partial Function
+let partialFunc = $.partial({(T: String...) in
+  T[0] + " " + T[1] + " from " + T[2]
+}, "Hello")
+print(partialFunc("World", "Swift"))
